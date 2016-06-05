ba9be64e9f248ea7393b4eb461d7d7ee30badcfc
radek <radoslaw.wichrowski@gmail.com>
2016-06-05 14:23:06 +0200

    Przykład do rozdziału AOP

--- src/main/java/s10338/aop/AppAOP.java
+++ src/main/java/s10338/aop/AppAOP.java
@@ -14,6 +14,5 @@ public class AppAOP {
         System.out.println("*************************");
         cust.printName();
         System.out.println("*************************");
-
     }
 }
--- src/main/java/s10338/aop/SomethingBeforeAdvice.java
+++ src/main/java/s10338/aop/SomethingBeforeAdvice.java
@@ -1,14 +1,13 @@
 package s10338.aop;
 
-
 import java.lang.reflect.Method;
 import org.springframework.aop.MethodBeforeAdvice;
 
-public class SomethingBeforeMethod implements MethodBeforeAdvice
+public class SomethingBeforeAdvice implements MethodBeforeAdvice
 {
     @Override
     public void before(Method method, Object[] args, Object target)
             throws Throwable {
-        System.out.println("SomethingBeforeMethod");
+        System.out.println("Something Before Advice");
     }
 }
